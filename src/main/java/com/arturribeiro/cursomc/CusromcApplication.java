2ff6f4827b35186cf16de99cc775a68faf1f3745
Artur Ribeiro <artur.adr@hotmail.com>
2019-09-18 14:11:59 -0300

    Projeto criado

---
+++ src/main/java/com/arturribeiro/cursomc/CusromcApplication.java
@@ -0,0 +1,13 @@
+package com.arturribeiro.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusromcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusromcApplication.class, args);
+	}
+
+}
