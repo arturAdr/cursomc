b2cc3dc50f9c81a6fe08f6d1d320622e2f05bf65
Artur Ribeiro <artur.adr@hotmail.com>
2019-09-19 14:35:07 -0300

    Tratativas de erros nos endpoints

---
+++ src/main/java/com/arturribeiro/cursomc/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.arturribeiro.cursomc.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
--- src/main/java/com/arturribeiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/arturribeiro/cursomc/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arturribeiro.cursomc.domain.Categoria;
+import com.arturribeiro.cursomc.exceptions.ObjectNotFoundException;
 import com.arturribeiro.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
